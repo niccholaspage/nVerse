1eade49859c2f514449363bb0ca437b2c33ec6c8
Nicholas Nassar <niccholaspage@gmail.com>
2013-09-17 20:45:22 -0400

    Better list command formatting

--- src/com/niccholaspage/nVerse/command/commands/ListCommand.java
+++ src/com/niccholaspage/nVerse/command/commands/ListCommand.java
@@ -1,6 +1,8 @@
 package com.niccholaspage.nVerse.command.commands;
 
 import org.bukkit.World;
+import org.bukkit.World.Environment;
+import org.bukkit.WorldType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -16,13 +18,43 @@ public class ListCommand extends SubCommand {
 
 		this.plugin = plugin;
 	}
-	
+
 	@Override
 	public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args){
+		int current = 1;
+		
+		String two = Phrase.SECONDARY_COLOR.parse();
+		
 		for (World world : plugin.getServer().getWorlds()){
-			sender.sendMessage(world.getName());
+			sender.sendMessage(two + current + ". " + Phrase.PRIMARY_COLOR.parse() + world.getName() + two + " - " + getEnvironment(world) + ", " + getWorldType(world));
+			
+			current += 1;
+		}
+
+		return true;
+	}
+	
+	private String getEnvironment(World world){
+		Environment environment = world.getEnvironment();
+		
+		if (Environment.THE_END.equals(environment)){
+			return "The End";
 		}
 		
-		return true;
+		String message = environment.name();
+		
+		message = message.substring(0, 1).toUpperCase() + message.substring(1).toLowerCase();
+		
+		return message;
+	}
+	
+	private String getWorldType(World world){
+		WorldType type = world.getWorldType();
+		
+		String message = type.getName();
+		
+		message = message.substring(0, 1).toUpperCase() + message.substring(1).toLowerCase();
+		
+		return message;
 	}
 }
